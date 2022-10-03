7ab3620971e526695e9479d9ddf22190dee4d0fc
Dikshant Agarwal <dikshant.agarwal@MACPRO380.local>
2022-10-03 13:52:29 +0530

    noithing done

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 module.exports = defineConfig({
   e2e: {
       setupNodeEvents(on, config) {
+        chromeWebSecurity: false
         allureWriter(on, config);
           return config;
       }
