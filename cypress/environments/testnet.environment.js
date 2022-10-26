668b295591f5574fa3afc4aca82b009b1a7585c8
Ashish Rana <dev.livelifelively@gmail.com>
2022-10-26 19:29:51 +0530

    formatting changes

--- cypress/environments/testnet.environment.js
+++ cypress/environments/testnet.environment.js
@@ -1,17 +1,18 @@
 const baseurl = 'https://payments.testnet.zoksh.com';
-const custAPIurl = 'https://api.testnet.zoksh.com'
-const custAPIep = '/api/v1/customers'
-const bearerToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzNlZGY2NmExYzI3MDQzNDk4Y2EzOTQiLCJuYW1lIjoiRGlrc2hhbnQgQWdhcndhbCIsImVtYWlsIjoiZGlrc2hhbnQuYWdhcndhbEBtb29wYXkubGl2ZSIsInZhbGlkVGlsbCI6MTY2NjAxOTE4NjcxMSwiaWF0IjoxNjY2MDE1NTg2fQ.qohciHVzC41sP7AheYH7cfZghBq-5eoxyOXSfgKcmJY'
+const custAPIurl = 'https://api.testnet.zoksh.com';
+const custAPIep = '/api/v1/customers';
+const bearerToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzNlZGY2NmExYzI3MDQzNDk4Y2EzOTQiLCJuYW1lIjoiRGlrc2hhbnQgQWdhcndhbCIsImVtYWlsIjoiZGlrc2hhbnQuYWdhcndhbEBtb29wYXkubGl2ZSIsInZhbGlkVGlsbCI6MTY2NjAxOTE4NjcxMSwiaWF0IjoxNjY2MDE1NTg2fQ.qohciHVzC41sP7AheYH7cfZghBq-5eoxyOXSfgKcmJY';
 
 const apiAuth = {
   key: '627b4a2836b59026e4d5cd6c',
-  secret: 'sk_test_EM+cYhQ50tBO31PAVEifGw=='
-}
+  secret: 'sk_test_EM+cYhQ50tBO31PAVEifGw==',
+};
 
 const versions = {
   versionTwo: 'v1',
-  versionOne: 'v2'
-}
+  versionOne: 'v2',
+};
 
 module.exports = {
   baseurl,
@@ -19,6 +20,5 @@ module.exports = {
   versions,
   custAPIurl,
   bearerToken,
-  custAPIep
-  
-}
+  custAPIep,
+};
