a1476136276268b352ccbf22665a6dbad9905634
Ashish Rana <dev.livelifelively@gmail.com>
2022-10-27 11:16:21 +0530

    reference types cypress

--- cypress/support/object-repo.js
+++ cypress/support/object-repo.js
@@ -1,3 +1,5 @@
+/// <reference types = "Cypress" />
+
 export const getProfileText = () => cy.get('p.profile__text__subheader');
 export const getNameField = () => cy.get('input[name="name"]');
 export const getEmailField = () => cy.get('input[name="email"]');
