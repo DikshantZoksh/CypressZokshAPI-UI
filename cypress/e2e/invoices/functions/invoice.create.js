99f0a741c129fd59d6e432b4744c22cf6519364b
Ashish Rana <dev.livelifelively@gmail.com>
2022-12-06 21:48:23 +0530

    promisify, reformat

--- cypress/e2e/invoices/functions/invoice.create.js
+++ cypress/e2e/invoices/functions/invoice.create.js
@@ -1,16 +1,9 @@
-/// <reference types = "Cypress" />
+// <reference types = "Cypress" />
 
-const cred = require('../ui/test-data/credentials.test-data');
-const invoice = require('../ui/test-data/invoice.create.test-data')
+const credentials = require('../ui/test-data/credentials.test-data');
+
+const invoice = require('../ui/test-data/invoice.create.test-data');
 const { truncateToDecimals } = require('../../../support/lib');
-let creation_dt_txt = ""
-//let invoice_number = ""
-let client_name_txt = ""
-let invoice_sts = ""
-let invoice_amount = ""
-let cust_email = ""
-let val = ""
-
 
 import {
   getEmailLogin,
@@ -18,206 +11,207 @@ import {
   getButtonLogin,
   getInvoiceLink,
   getCreateInvoiceButton,
-  getcurrencydropdown,
+  getCurrencyDropdown,
   getDescriptioninput,
   getQuantity,
-  getunitprice,
-  gettaxpercent,
+  getUnitPrice,
+  getTaxPercent,
   getAmtwithoutTax,
   getTaxAmount,
   getTotalAmount,
-  getGenerateInvoiceButton,
+  // getGenerateInvoiceButton,
   getSelectPayeeButton,
   getCustomerListTable,
-  getdateIssuedOn,
-  getdatePaymentdueBy
+  getDateIssuedOn,
+  getDatePaymentDueBy,
 } from '../../../support/object-repo';
 
-
-//Get customer email from the customer table 
-export function getCustomerEmail(){
-  getCustomerListTable()
-    .find('tr')
-    .eq(1)
-    .find('td')
-    .eq(1)
-    .then(($el) => {
-      cust_email = $el.text()
-      cy.log("Customer Email is : " + cust_email)
-    })
-  return cust_email
-}
-
-
-
-// Check if any payee exist in the payee information table
-export function CheckforPayeeExist(){
-  getInvoiceLink().click({ force: true });
-  cy.url().should('include', '/invoices');
-  getCreateInvoiceButton().click({ force: true });
-  cy.url().should('include', '/invoices/create');
-  cy.wait(2000);
-  cy.get('h1').contains('Create Invoice');
-  getSelectPayeeButton().click({ force: true });
-  getCustomerListTable()
-      .find('tr')
-      .then((row) => {
-    let row_count = row.length
-    if(row_count >= 2) {
-      return true
-    }else{
-      return false
-    }
-  });
-}
-
-
-// Login to the DashBoard
-  export function Logindashboard(){
-    cy.visit(cred.app_url)
-    getEmailLogin().type(cred.username).should('have.value', cred.username);
-    getPasswordLogin().type(cred.password).should('have.value', cred.password);
-    getButtonLogin().click();
-    cy.url().should('include','/dashboard')  
-  };
-
-//Select the first customer from the table
-  export function selectfirstcustomer(){
+// Get customer email from the customer table
+export async function getCustomerEmail() {
+  return new Promise((resolve, reject) => {
     getCustomerListTable()
       .find('tr')
       .eq(1)
       .find('td')
-      .eq(3)
-      .find("button[aria-label='Select Payee']").click({ force: true })
-  }  
-      
-  //Fill invoice details 
-  export function Setinvoicedetails(){
-    getdateIssuedOn().clear();
-    getdateIssuedOn().type(invoice.issuedon).should('have.value',invoice.issuedon)
-    getdatePaymentdueBy().clear();
-    getdatePaymentdueBy().type(invoice.paymentdueby).should('have.value',invoice.paymentdueby)
-    getcurrencydropdown().click({ force: true });
-    cy.get('li')
-      .contains(invoice.Currency)
-      .click({ force: true });
-    getDescriptioninput().clear();
-    getDescriptioninput()
-      .type(invoice.description)
-      .should('have.value', invoice.description);
-    getQuantity().clear();
-    getQuantity()
-      .type(invoice.Qty)
-      .should('have.value', invoice.Qty);
-    getunitprice().clear();
-    getunitprice()
-      .type(invoice.unitprice)
-      .should('have.value', invoice.unitprice);
-    gettaxpercent().clear();
-    gettaxpercent()
-      .type(invoice.tax)
-      .should('have.value', invoice.tax);
-    cy.log('Tax %  : ' + invoice.tax);
-    cy.log('Quanity : ' + invoice.Qty);
-    cy.log('Unit Price : ' + invoice.unitprice);
-    let amount_withoutTax = invoice.Qty * invoice.unitprice;
-    amount_withoutTax = truncateToDecimals(amount_withoutTax);
-    cy.log('Amount without Tax : ' + amount_withoutTax);
-    let tax_amount = (invoice.tax / 100) * amount_withoutTax;
-    tax_amount = truncateToDecimals(tax_amount);
-    cy.log('Tax amount : ' + tax_amount);
-    let total_amount = amount_withoutTax + tax_amount;
-    total_amount = truncateToDecimals(total_amount);
-
-    cy.log('Total Amount : ' + total_amount);
-    getAmtwithoutTax().should('have.value', amount_withoutTax);
-    getTaxAmount().should('have.value', tax_amount);
-    getTotalAmount().should('have.value', total_amount); 
-
-  }
-
-  //View the last created invoice from the table
-  export function viewlastcreatedinvoice(){
-    cy.get("Table[role='table']")
-      .find('tr')
       .eq(1)
-      .find('td')
-      .eq(5)
-      .find('a').click({ force: true })
-  }  
+      .then(($el) => {
+        const emailAddress = $el.text();
+        cy.log('Customer Email is : ' + emailAddress);
+        resolve(emailAddress);
+      });
+  });
+}
+
+// Check if any payee exist in the payee information table
+export async function checkIfPayeeExist() {
+  getInvoiceLink().click({ force: true, multiple: true });
+  cy.url().should('include', '/invoices');
+
+  getCreateInvoiceButton().click({ force: true });
+  cy.url().should('include', '/invoices/create');
+
+  // cy.wait(2000);
+  cy.get('h1').contains('Create Invoice');
+  getSelectPayeeButton().click({ force: true });
+
+  return new Promise((resolve, reject) => {
+    getCustomerListTable()
+      .find('tr')
+      .then((row) => {
+        let row_count = row.length;
+        if (row_count >= 2) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      });
+  });
+}
+
+// Login to the DashBoard
+export function loginDashboard() {
+  const { app_url, username, password } = credentials;
+
+  cy.visit(app_url);
+
+  getEmailLogin().type(username).should('have.value', username);
+  getPasswordLogin().type(password).should('have.value', password);
+  getButtonLogin().click();
+
+  cy.url().should('include', '/dashboard');
+}
+
+//Select the first customer from the table
+export function selectFirstCustomer() {
+  getCustomerListTable()
+    .find('tr')
+    .eq(1)
+    .find('td')
+    .eq(3)
+    .find("button[aria-label='Select Payee']")
+    .click({ force: true });
+}
+
+// Fill invoice details
+export function setInvoiceDetails() {
+  getDateIssuedOn().clear();
+  getDateIssuedOn().type(invoice.issuedon).should('have.value', invoice.issuedon);
+  getDatePaymentDueBy().clear();
+  getDatePaymentDueBy().type(invoice.paymentdueby).should('have.value', invoice.paymentdueby);
+  getCurrencyDropdown().click({ force: true });
+  cy.get('li').contains(invoice.Currency).click({ force: true });
+  getDescriptioninput().clear();
+  getDescriptioninput().type(invoice.description).should('have.value', invoice.description);
+  getQuantity().clear();
+  getQuantity().type(invoice.Qty).should('have.value', invoice.Qty);
+  getUnitPrice().clear();
+  getUnitPrice().type(invoice.unitprice).should('have.value', invoice.unitprice);
+  getTaxPercent().clear();
+  getTaxPercent().type(invoice.tax).should('have.value', invoice.tax);
+
+  cy.log('Tax %  : ' + invoice.tax);
+  cy.log('Quanity : ' + invoice.Qty);
+  cy.log('Unit Price : ' + invoice.unitprice);
+
+  let amount_withoutTax = invoice.Qty * invoice.unitprice;
+  amount_withoutTax = truncateToDecimals(amount_withoutTax);
+
+  cy.log('Amount without Tax : ' + amount_withoutTax);
+
+  let tax_amount = (invoice.tax / 100) * amount_withoutTax;
+  tax_amount = truncateToDecimals(tax_amount);
+
+  cy.log('Tax amount : ' + tax_amount);
+
+  let total_amount = amount_withoutTax + tax_amount;
+  total_amount = truncateToDecimals(total_amount);
+
+  cy.log('Total Amount : ' + total_amount);
+  getAmtwithoutTax().should('have.value', amount_withoutTax);
+  getTaxAmount().should('have.value', tax_amount);
+  getTotalAmount().should('have.value', total_amount);
+}
+
+//View the last created invoice from the table
+export function viewLastCreatedInvoice() {
+  cy.get("Table[role='table']").find('tr').eq(1).find('td').eq(5).find('a').click({ force: true });
+}
 
 // Get creation date from the last created invoice on the invoice table
-  export function getCreationDate(){
+export async function getCreationDate() {
+  return new Promise((resolve, reject) => {
     cy.get("Table[role='table']")
       .find('tr')
       .eq(1)
       .find('td')
       .eq(0)
       .then(($btn) => {
-        creation_dt_txt = $btn.text()
-        cy.log("creation date is : " + creation_dt_txt)
-      })
-    return creation_dt_txt
-  }
+        const creationDate = $btn.text();
+        cy.log('Creation date: ' + creationDate);
+        resolve(creationDate);
+      });
+  });
+}
 
 //Get invoice number from the last created invoice on the invoice table
-  export function getinvoicenumber(){
+export async function getInvoiceNumber() {
+  return new Promise((resolve, reject) => {
     cy.get("Table[role='table']")
       .find('tr')
       .eq(1)
       .find('td')
       .eq(1)
       .then(($btn) => {
-        //cy.log("invoice number is : " + $btn.text())
-        const val = $btn.text()
-        return val        
-      })
-  }
+        const val = $btn.text();
+        resolve(val);
+      });
+  });
+}
 
-  //Get client name from the last created invoice on the invoice table
-  export function getClientname(){
+//Get client name from the last created invoice on the invoice table
+export async function getClientName() {
+  return new Promise((resolve, reject) => {
     cy.get("Table[role='table']")
       .find('tr')
       .eq(1)
       .find('td')
       .eq(2)
       .then(($btn) => {
-        client_name_txt = $btn.text()
-        cy.log("Client name  is : " + client_name_txt)
-      })
-    return client_name_txt
-  }
+        const clientNameText = $btn.text();
+        cy.log('Client name  is : ' + clientNameText);
+        resolve(clientNameText);
+      });
+  });
+}
 
-  //Get invoice status from the last created invoice on the invoice table
-  export function getinvoicestatus(){
+//Get invoice status from the last created invoice on the invoice table
+export async function getInvoiceStatus() {
+  return new Promise((resolve, reject) => {
     cy.get("Table[role='table']")
       .find('tr')
       .eq(1)
       .find('td')
       .eq(3)
       .then(($btn) => {
-        invoice_sts = $btn.text()
-        cy.log("Invoice  status is : " + invoice_sts)
-      })
-    return invoice_sts
-  }
+        const invoiceStatus = $btn.text();
+        cy.log('Invoice  status is : ' + invoiceStatus);
+        resolve(invoiceStatus);
+      });
+  });
+}
 
-  //Get invoice amount date from the last created invoice on the invoice table
-  export function getinvoiceAmount(){
+//Get invoice amount date from the last created invoice on the invoice table
+export async function getInvoiceAmount() {
+  return new Promise((resolve, reject) => {
     cy.get("Table[role='table']")
       .find('tr')
       .eq(1)
       .find('td')
       .eq(4)
       .then(($btn) => {
-        invoice_amount = $btn.text()
-        cy.log("Invoice amount is : " + invoice_amount)
-      })
-    return invoice_amount
-  }
-
-
-
-
-    
-  
+        const invoiceAmount = $btn.text();
+        cy.log('Invoice amount is : ' + invoiceAmount);
+        resolve(invoiceAmount);
+      });
+  });
+}
