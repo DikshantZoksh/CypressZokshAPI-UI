9f306293073ae5b74bbe9312d0709279ca722d2d
Dikshant Agarwal <dikshant.agarwal@MACPRO380.local>
2023-01-19 20:08:40 +0530

    Uncomment a code in ramps

--- cypress/e2e/Ramps/test/ON/on-ramp.cy.js
+++ cypress/e2e/Ramps/test/ON/on-ramp.cy.js
@@ -15,42 +15,42 @@ describe('Ramp On aggregator validation', () => {
     cy.url().should('include', '/ramps');
   });
 
-  // it('Validate Fiat currency and currency sign', () => {
+  it('Validate Fiat currency and currency sign', () => {
 
-  //   //Select fiat as EUR
-  //   validateCurrandSymbol('EUR','€')
-  //   //Select fiat as USD
-  //   validateCurrandSymbol('USD','$')
-  //   //Select fiat as GBP
-  //   validateCurrandSymbol('GBP','£')
-  //    //Select fiat as CHF
-  //   validateCurrandSymbol('CHF','CHF')
-  //   //Select fiat as SEK
-  //   validateCurrandSymbol('SEK','kr')
-  //   //Select fiat as PLN
-  //   validateCurrandSymbol('PLN','zł')
-  //   //Select fiat as NOK
-  //   validateCurrandSymbol('NOK','kr')
-  //   //Select fiat as DKK
-  //   validateCurrandSymbol('DKK','kr.')
-  //   //Select fiat as NZD
-  //   validateCurrandSymbol('NZD','$')
-  //   //Select fiat as NOK
-  //   validateCurrandSymbol('MXN','$')
-  //   //Select fiat as CAD
-  //   validateCurrandSymbol('CAD','CA$')
-  //   //Select fiat as AUD
-  //   validateCurrandSymbol('AUD','AU$')
-  //   //Select fiat as BRL
-  //   validateCurrandSymbol('BRL','R$')
-  //   //Select fiat as AUD
-  //   validateCurrandSymbol('AUD','AU$')
-  //   //Select fiat as AUD
-  //   validateCurrandSymbol('AUD','AU$')
-  //   //Select fiat as CLP
-  //   validateCurrandSymbol('CLP','CLP$')
+    //Select fiat as EUR
+    validateCurrandSymbol('EUR','€')
+    //Select fiat as USD
+    validateCurrandSymbol('USD','$')
+    //Select fiat as GBP
+    validateCurrandSymbol('GBP','£')
+     //Select fiat as CHF
+    validateCurrandSymbol('CHF','CHF')
+    //Select fiat as SEK
+    validateCurrandSymbol('SEK','kr')
+    //Select fiat as PLN
+    validateCurrandSymbol('PLN','zł')
+    //Select fiat as NOK
+    validateCurrandSymbol('NOK','kr')
+    //Select fiat as DKK
+    validateCurrandSymbol('DKK','kr.')
+    //Select fiat as NZD
+    validateCurrandSymbol('NZD','$')
+    //Select fiat as NOK
+    validateCurrandSymbol('MXN','$')
+    //Select fiat as CAD
+    validateCurrandSymbol('CAD','CA$')
+    //Select fiat as AUD
+    validateCurrandSymbol('AUD','AU$')
+    //Select fiat as BRL
+    validateCurrandSymbol('BRL','R$')
+    //Select fiat as AUD
+    validateCurrandSymbol('AUD','AU$')
+    //Select fiat as AUD
+    validateCurrandSymbol('AUD','AU$')
+    //Select fiat as CLP
+    validateCurrandSymbol('CLP','CLP$')
 
-  // });
+  });
 
   it('Select fiat and token for exchange', () => {
   validateCurrandSymbol('USD','$')
